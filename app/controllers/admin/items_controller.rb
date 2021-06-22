e88bfbf5b6f3c6141d74d14cf18a0d8dd0cf77bf
toshi004 <d.toshi004.35@gmail.com>
2021-06-22 09:04:48 +0000

    [Add]商品新規登録ページの作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Items.new
   end
 
   def create
+    @item = Items.new(item_params)
+    @item.save
+    redirect_to admin_item_path
   end
 
   def show
@@ -16,4 +20,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  priavte
+
+  def item_params
+    params.require(:item).permit(:name, :image_id, :introduction, :price, :is_active)
+  end
+
 end
