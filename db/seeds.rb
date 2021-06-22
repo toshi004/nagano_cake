b2e25e8f04c2274cd06ba3e3e9b56dce3a64a3f0
toshi004 <d.toshi004.35@gmail.com>
2021-06-22 02:57:15 +0000

    [Update]viewページの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Admin.create(
-  :id => '1',
-  :email => 'admin@naganocake.com',
-  :encrypted_password => 'adminpass'
-)
