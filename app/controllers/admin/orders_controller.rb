5a245bf4c6f3112064808bdb329e0f64f5f306da
toshi004 <d.toshi004.35@gmail.com>
2021-06-24 14:34:38 +0000

    [Add]注文履歴詳細画面の追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
-
+    @order = Order.find(params[:id])
+    @order_items = OrderItem.find(params[:id])
     render "admin/order_items/update"
   end
 
