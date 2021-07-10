f21524e8790fc119f8bd9e26520cecd20d362b78
toshi004 <d.toshi004.35@gmail.com>
2021-07-10 14:58:47 +0000

    [Update]購入機能の実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -47,6 +47,6 @@ class Public::CartItemsController < ApplicationController
     private
 
     def cart_item_params
-        params.require(:cart_item).permit(:amount, :item_id)
+        params.require(:cart_item).permit(:amount, :item_id, :customer_id)
     end
 end
